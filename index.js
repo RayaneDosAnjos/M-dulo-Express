2c0d7c07e5cccb028483d05fe7f76d75c674c7df
RayaneDosAnjos <raydosanjos98@gmail.com>
2023-02-25 23:04:22 -0300

    arquivos estáticos com express, e instalação do nodemon

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
+
+app.use(express.static(path.join(__dirname, 'cliente')))
+
+
 
 const PORT = 5000
 app.listen(PORT, ()=>{
