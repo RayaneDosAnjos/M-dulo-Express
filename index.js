4623aa4f3d723e5edb293e63813de1d3526c8bab
RayaneDosAnjos <raydosanjos98@gmail.com>
2023-02-26 00:50:54 -0300

    conhecendo os métodos get, post, put, delete

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'cliente')))
+app.use('/meusite',express.static(path.join(__dirname, 'cliente')))
 
+app.get("/", (req,res) => {
+     
+    res.send("<h1>Hello World from GET</h1>")
+})
 
+app.post("/", (req, res) => {
+    
+    res.send("<h1>Hello World from POST</h1>")
+})
+
+app.put("/", (req, res) => {
+
+    res.send("<h1>Hello World from PUT</h1>")
+})
+
+app.delete("/", (req, res) => {
+
+    res.send("<h1>Hello World from DELETE</h1>")
+})
 
 const PORT = 5000
 app.listen(PORT, ()=>{
-    console.log(`server running on port:${PORT}`)
+    console.log(`server running on port:${PORT} in link http://localhost:${PORT}`)
     
 })
